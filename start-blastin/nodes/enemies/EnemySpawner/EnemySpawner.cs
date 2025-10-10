f08366fff430332eb6852803d610675b6c672c90
PolyrhythmicDrop <161067622+PolyrhythmicDrop@users.noreply.github.com>
2025-10-10 11:37:31 -0700

    spawner now creates enemies at regular intervals along a path

--- start-blastin/nodes/enemies/EnemySpawner/EnemySpawner.cs
+++ start-blastin/nodes/enemies/EnemySpawner/EnemySpawner.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Linq;
+using System.Reflection;
 using Factories;
 using Godot;
 
@@ -11,6 +11,7 @@ namespace Enemies
         private Path2D _path;
         private PathFollow2D _pathFollow;
         private Curve2D _curve;
+        private float _pointMoveDuration;
         private float _spawnInterval;
         private Timer _spawnTimer;
         private Godot.Collections.Dictionary<EnemyResource, int> _enemyPool;
@@ -36,9 +37,23 @@ namespace Enemies
         }
 
         /// <summary>
+        /// The time it takes for the spawn point to move to the end of the Path2D curve.
+        /// Higher values result in a slower-moving spawn point.
+        /// </summary>
+        /// <remarks>
+        /// Used to set the duration parameter of the spawn point move Tween.
+        /// </remarks>
+        [Export]
+        public float SpawnPointMoveDuration
+        {
+            get => _pointMoveDuration;
+            set => _pointMoveDuration = value;
+        }
+
+        /// <summary>
         /// Weighted pool of enemies that the spawn point can spawn. The key is the type of enemy, the value is the weighted value of that enemy.
         /// </summary>
-        [Export(PropertyHint.ResourceType, "EnemyResource")]
+        [Export]
         public Godot.Collections.Dictionary<EnemyResource, int> EnemyPool
         {
             get => _enemyPool;
@@ -47,8 +62,11 @@ namespace Enemies
 
         public override void _Ready()
         {
+            GD.Print(
+                $"{MethodBase.GetCurrentMethod().ReflectedType}.{MethodBase.GetCurrentMethod().Name} called!"
+            );
             _path = GetNode<Path2D>("%Path2D");
-            _path.Curve = _curve;
+            // _path.Curve = _curve;
 
             _pathFollow = _path.GetNode<PathFollow2D>("%PathFollow2D");
             _spawnPoint = _pathFollow.GetNode<Node2D>("%SpawnPoint");
@@ -57,10 +75,19 @@ namespace Enemies
             _spawnTimer = GetNode<Timer>("%SpawnTimer");
             _spawnTimer.WaitTime = _spawnInterval;
             _spawnTimer.Timeout += SpawnEnemy;
+            _spawnTimer.Start();
+
+            MoveSpawnPoint();
+        }
+
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
         }
 
         private EnemyResource GetEnemyFromPool()
         {
+            GD.Print($"{MethodBase.GetCurrentMethod().Name} -> \n");
             if (_enemyPool == null || _enemyPool.Count == 0)
                 return null;
 
@@ -71,8 +98,12 @@ namespace Enemies
                 totalWeight += num;
             }
 
+            GD.Print($"Total weight: {totalWeight}");
+
             // Generate random number within total weight
             int randomValue = GD.RandRange(0, totalWeight - 1);
+
+            GD.Print($"Random value: {randomValue}");
 
             // Find the enemy that corresponds to this weight
             int currentWeight = 0;
@@ -81,20 +112,41 @@ namespace Enemies
                 currentWeight += kvp.Value;
                 if (randomValue < currentWeight)
                 {
+                    GD.Print($"Returning {kvp.Key} from foreach loop!");
                     return kvp.Key;
                 }
             }
 
             // Fallback
+            GD.Print(
+                $"Something went wrong! Returning first key in enemy pool, which is: {_enemyPool.Keys.First()}"
+            );
             return _enemyPool.Keys.First();
         }
 
         private void SpawnEnemy()
         {
+            GD.Print($"{MethodBase.GetCurrentMethod().Name}: Attempting to spawn enemy...");
             EnemyResource enemyResource = GetEnemyFromPool();
             EnemyNode enemy = EnemyFactory.CreateEnemy(enemyResource);
+
+            GD.Print($"{MethodBase.GetCurrentMethod().Name}: {enemy.Name} created from factory!");
             enemy.GlobalPosition = _spawnPoint.GlobalPosition;
+            GD.Print(
+                $"{MethodBase.GetCurrentMethod().Name}: {enemy} global position set to {enemy.GlobalPosition}."
+            );
             _spawnParent.AddChild(enemy);
+            GD.Print(
+                $"{MethodBase.GetCurrentMethod().Name}: {enemy} added to tree! Enemy global position: {enemy.GlobalPosition}."
+            );
+        }
+
+        private void MoveSpawnPoint()
+        {
+            Tween tween = CreateTween();
+            tween.TweenProperty(_pathFollow, "progress_ratio", 1.0, _pointMoveDuration);
+            tween.TweenProperty(_pathFollow, "progress_ratio", 0, _pointMoveDuration);
+            tween.SetLoops();
         }
     }
 }
